76ac6979ffc0b1f27cc71c77de4e6d9e5a37659e
Minseo Choi <minseochoi0308@gmail.com>
2021-04-23 15:37:53 +0900

    #6 증감연산자 예제 연습

--- CSBBasic/Program.cs
+++ CSBBasic/Program.cs
@@ -150,6 +150,16 @@ namespace CSBBasic
 
             bool IsLine = true;
 
+
+            //증감 연산자 - 섞어서 쓸 경우
+            //전위, 호위에 따라서 출력 값이 변할 수 있으니 주의
+
+            int num = 10;
+            Console.WriteLine(num++);
+            Console.WriteLine(++num);
+            Console.WriteLine(num--);
+            Console.WriteLine(--num);
+
         }
     }
 }
