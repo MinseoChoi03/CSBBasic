82fb4d1eff21981c61e10b52fb4474204fb07281
Minseo Choi <minseochoi0308@gmail.com>
2021-08-31 10:51:54 +0900

    제너릭 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -8,8 +8,31 @@ namespace CSBasic7
 {
     class Program
     {
+        class Wanted<T>
+            //where T : Student
+        {
+            public T Value;
+            public Wanted(T value)
+            {
+                this.Value = value;
+            }
+        }
+        class Student
+        {
+            public override string ToString()
+            {
+                return "학생";
+            }
+        }
         static void Main(string[] args)
         {
+            Wanted<string> ws = new Wanted<string>("string");
+            Wanted<int> wi = new Wanted<int>(10);
+            Wanted<Student> wstu = new Wanted<Student>(new Student());
+            Console.WriteLine(ws);
+            Console.WriteLine(wi);
+            Console.WriteLine(wstu);
+
         }
     }
 }
